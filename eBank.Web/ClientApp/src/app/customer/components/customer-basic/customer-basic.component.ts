c931bb9dc84b5e4dcb9015c335025f811dbf9e94
Ehsan Shafiei <EhsanS@sedata.com>
2019-09-11 10:40:03 -0400

    uses renderer2 to set focus on first name in customer form

--- eBank.Web/ClientApp/src/app/customer/components/customer-basic/customer-basic.component.ts
+++ eBank.Web/ClientApp/src/app/customer/components/customer-basic/customer-basic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { LegalStatus } from '../../../shared/enums/legal-status.enum';
 import { MaritalStatus } from '../../../shared/enums/marital-status.enum';
@@ -14,13 +14,14 @@ export class CustomerBasicComponent implements OnInit {
   legalStatusItems: string[] = [];
   maritalStatus: typeof MaritalStatus = MaritalStatus;
   maritalStatusItems: string[] = [];
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
     const legalStatusOptions = Object.keys(LegalStatus);
     this.legalStatusItems = legalStatusOptions.slice(legalStatusOptions.length / 2);
     const maritalStatusOptions = Object.keys(MaritalStatus);
     this.maritalStatusItems = maritalStatusOptions.slice(maritalStatusOptions.length / 2);
+    this.renderer.selectRootElement('#firstName', true).focus();
   }
 
   get f() {
