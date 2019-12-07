cd1d723d781b725f6cd1ffc9ef985d0f5c731c72
Ehsan Shafiei <EhsanS@sedata.com>
2019-12-07 09:33:43 -0500

    adds unit test project shell

---
+++ eBank.DataAccess/Migrations/20191207142114_add-transfers-table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eBank.DataAccess.Migrations
+{
+    public partial class addtransferstable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transfers",
+                columns: table => new
+                {
+                    TransferId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OriginAccount = table.Column<long>(nullable: false),
+                    DestinationAccount = table.Column<long>(nullable: false),
+                    Frequency = table.Column<int>(nullable: false),
+                    TransferDate = table.Column<DateTime>(nullable: false),
+                    Amount = table.Column<double>(nullable: false),
+                    Note = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transfers", x => x.TransferId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transfers");
+        }
+    }
+}
---
+++ eBank.DataAccess/Models/Transfer/TransferModel.cs
@@ -0,0 +1,24 @@
+﻿using eBank.DataAccess.Models.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eBank.DataAccess.Models.Transfer
+{
+    public class TransferModel : BaseModel
+    {
+        [Key]
+        public int TransferId { get; set; }
+
+        public long OriginAccount { get; set; }
+
+        public long DestinationAccount { get; set; }
+
+        public int Frequency { get; set; }
+
+        public DateTime TransferDate { get; set; }
+
+        public double Amount { get; set; }
+
+        public string Note { get; set; }
+    }
+}
